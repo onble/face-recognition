d0a624dba919d933d99406ff13265505aa68c4ea
Li_HaoBo <1546909339@qq.com>
2022-12-20 00:09:49 +0800

    完成待办事项的前端逻辑

--- src/main/resources/static/js/todo_edit.js
+++ src/main/resources/static/js/todo_edit.js
@@ -1,4 +1,12 @@
 // 工具函数
+function clearbox(box) {
+    // 清除一个盒子中的所有子节点
+
+    const childs = box.childNodes;
+    for (i = childs.length - 1; i >= 0; i--) {
+        box.removeChild(childs[i]);
+    }
+}
 // 下面JS函数用于获取url参数
 function getQueryVariable(variable) {
     let query = window.location.search.substring(1);
@@ -11,6 +19,74 @@ function getQueryVariable(variable) {
         return false;
     }
 }
+function reder_edit_data(data) {
+    // 修改标题
+    const title_input = document.querySelector("input[name='title']");
+    title_input.value = data["title"];
+    // 修改内容
+    const content_input = document.querySelector("textarea[name='content']");
+    content_input.value = data["content"];
+    // 修改状态
+    if (data["status"] == true) {
+        // 去选择已完成
+        const over_input = document.querySelector(
+            "input[name='status'][value='1']"
+        );
+        const not_input = document.querySelector(
+            "input[name='status'][value='0']"
+        );
+        const status_box = document.querySelector("#status_box");
+        // clearbox(status_box);
+        html = `
+        <label class="layui-form-label">
+                                    <span class="x-red">*</span>状态
+                                </label>
+                                <div class="layui-input-block">
+                                    <input
+                                        type="radio"
+                                        name="status"
+                                        value="1"
+                                        title="已完成"
+                                        checked="checked"
+                                    />
+                                    <div
+                                        class="layui-unselect layui-form-radio layui-form-radioed"
+                                    >
+                                        <i
+                                            class="layui-anim layui-icon layui-anim-scaleSpring"
+                                            ></i
+                                        >
+                                        <div>已完成</div>
+                                    </div>
+                                    <input
+                                        type="radio"
+                                        name="status"
+                                        value="0"
+                                        title="未完成"
+                                        
+                                    />
+                                    <div
+                                        class="layui-unselect layui-form-radio"
+                                    >
+                                        <i class="layui-anim layui-icon"></i>
+                                        <div>未完成</div>
+                                    </div>
+                                </div>
+        `;
+        // status_box.innerHTML = html;
+        $("[name=status]").each(function (i, item) {
+            if ($(item).val() == "1") {
+                //更改选中值
+                $(item).prop("checked", true);
+                //重新渲染
+                layui.use("form", function () {
+                    form = layui.form();
+                    form.render();
+                });
+            }
+        });
+    }
+}
 function init() {
     // 异步请求获取数据
     // 创建ajax进行传递数据
@@ -21,6 +97,9 @@ function init() {
     // 3.发送
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //这行代码很关键，用来把字符串类型的参数序列化成Form Data
     xhr.send(`id=${getQueryVariable("id")}`);
+    // 将id传送给输入框存储
+    document.querySelector("input[name=todo_id]").value =
+        getQueryVariable("id");
     // 后台成功接收到传输的数据
     // 4.事件绑定
     xhr.onreadystatechange = function () {
@@ -28,7 +107,7 @@ function init() {
             if (xhr.status >= 200 && xhr.status < 300) {
                 const result = JSON.parse(xhr.responseText);
                 // 重新渲染页面数据
-                console.log("返回的数据哦：", result);
+                reder_edit_data(result["todo_inf"]);
             }
         }
     };

1c3adad07ac4f792b72a5da4a0e0fffcab38c74e
Li_HaoBo <1546909339@qq.com>
2024-06-12 10:49:43 +0800

    进度存储

--- LHB/init_test.sql
+++ LHB/init_test.sql
@@ -30,15 +30,25 @@ create table people_base_info
     imageNum int         not null default 0 comment '题库照片数量',
     status   tinyint     not null default 0 comment '状态，0：正常使用 1:禁止使用'
 ) comment '人物基础信息表';
-insert into people_base_info (id, name, gender, imageNum)
-values (1, '李晨', 1, 12),
-       (2, '鹿晗', 1, 16),
-       (3, '郑恺', 1, 20),
-       (4, 'Angelagbaby', 0, 8),
-       (5, '迪丽热巴', 0, 36),
-       (6, '范冰冰', 0, 9),
-       (7, '杨幂', 0, 46),
-       (8, '赵丽颖', 0, 9);
+insert into people_base_info (name, gender, imageNum)
+values ('李晨', 1, 12),
+       ('陈立农', 1, 101),
+       ('王俊凯', 1, 100),
+       ('王一博', 1, 100),
+       ('许光汉', 1, 84),
+       ('薛之谦', 1, 104),
+       ('张艺兴', 1, 100),
+       ('朱一龙', 1, 100),
+#        ('鹿晗', 1, 16),
+       ('郑恺', 1, 54),
+       ('Angelagbaby', 0, 164),
+       ('迪丽热巴', 0, 104),
+       ('范冰冰', 0, 102),
+       ('古力娜扎', 0, 107),
+       ('杨幂', 0, 196),
+       ('张子枫', 0, 103),
+       ('赵今麦', 0, 103),
+       ('赵丽颖', 0, 100);
 
 -- 生成题目记录表
 create table test_base_info
@@ -51,35 +61,7 @@ create table test_base_info
     right_times int                   default 0 comment '做对次数',
     foreign key (people_id) references people_base_info (id)
 ) comment '题目记录表';
-insert into test_base_info (people_id, imageIndex)
-values (1, 'public/face/李晨/1.jpg'),
-       (1, 'public/face/李晨/2.jpg'),
-       (1, 'public/face/李晨/3.jpg'),
-       (1, 'public/face/李晨/4.jpg'),
-       (1, 'public/face/李晨/5.jpg'),
-       (1, 'public/face/李晨/6.jpg'),
-       (1, 'public/face/李晨/7.jpg'),
-       (1, 'public/face/李晨/8.jpg'),
-       (1, 'public/face/李晨/9.jpg'),
-       (1, 'public/face/李晨/10.jpg'),
-       (1, 'public/face/李晨/11.jpg'),
-       (1, 'public/face/李晨/12.jpg'),
-       (2, 'public/face/鹿晗/1.jpg'),
-       (2, 'public/face/鹿晗/2.jpg'),
-       (2, 'public/face/鹿晗/3.jpg'),
-       (2, 'public/face/鹿晗/4.jpg'),
-       (2, 'public/face/鹿晗/5.jpg'),
-       (2, 'public/face/鹿晗/6.jpg'),
-       (2, 'public/face/鹿晗/7.jpg'),
-       (2, 'public/face/鹿晗/8.jpg'),
-       (2, 'public/face/鹿晗/9.jpg'),
-       (2, 'public/face/鹿晗/10.jpg'),
-       (2, 'public/face/鹿晗/11.jpg'),
-       (2, 'public/face/鹿晗/12.jpg'),
-       (2, 'public/face/鹿晗/13.jpg'),
-       (2, 'public/face/鹿晗/14.jpg'),
-       (2, 'public/face/鹿晗/15.jpg'),
-       (2, 'public/face/鹿晗/16.jpg');
+
 
 -- 生成四选一题组信息表
 create table four_test_info
@@ -95,15 +77,6 @@ create table four_test_info
     foreign key (test3_id) references test_base_info (id),
     foreign key (test4_id) references test_base_info (id)
 ) comment '四选一题组信息表';
-insert into four_test_info (test1_id, test2_id, test3_id, test4_id)
-values (1, 2, 3, 4),
-       (1, 3, 2, 4),
-       (1, 3, 4, 5),
-       (2, 3, 4, 5),
-       (2, 4, 5, 6),
-       (1, 4, 5, 6),
-       (2, 5, 3, 1),
-       (2, 3, 4, 1);
 
 
 -- 生成四选一题目操作表
@@ -131,20 +104,20 @@ create table user_test
 -- 生成分类测试题组信息表
 create table classification_test_info
 (
-    id       int primary key auto_increment comment '分类题组id',
-    A_id int comment '人物A',
-    B_id int comment '人物B',
-    test1_id int comment '测试题1',
-    test2_id int comment '测试题2',
-    test3_id int comment '测试题3',
-    test4_id int comment '测试题4',
-    test5_id int comment '测试题5',
-    test6_id int comment '测试题6',
-    test7_id int comment '测试题7',
-    test8_id int comment '测试题8',
-    test9_id int comment '测试题9',
+    id        int primary key auto_increment comment '分类题组id',
+    A_id      int comment '人物A',
+    B_id      int comment '人物B',
+    test1_id  int comment '测试题1',
+    test2_id  int comment '测试题2',
+    test3_id  int comment '测试题3',
+    test4_id  int comment '测试题4',
+    test5_id  int comment '测试题5',
+    test6_id  int comment '测试题6',
+    test7_id  int comment '测试题7',
+    test8_id  int comment '测试题8',
+    test9_id  int comment '测试题9',
     test10_id int comment '测试题10',
-    status   tinyint not null default 0 comment '状态，0：正常使用 1:禁止使用',
+    status    tinyint not null default 0 comment '状态，0：正常使用 1:禁止使用',
     foreign key (A_id) references test_base_info (id),
     foreign key (B_id) references test_base_info (id),
     foreign key (test1_id) references test_base_info (id),
@@ -161,32 +134,32 @@ create table classification_test_info
 -- 生成分类测试题目操作表
 create table classification_test_action
 (
-    id      int primary key auto_increment comment '分类题目操作id',
-    action1 int not null default 0 comment '操作1',
-    action2 int not null default 0 comment '操作2',
-    action3 int not null default 0 comment '操作3',
-    action4 int not null default 0 comment '操作4',
-    action5 int not null default 0 comment '操作5',
-    action6 int not null default 0 comment '操作6',
-    action7 int not null default 0 comment '操作7',
-    action8 int not null default 0 comment '操作8',
-    action9 int not null default 0 comment '操作9',
+    id       int primary key auto_increment comment '分类题目操作id',
+    action1  int not null default 0 comment '操作1',
+    action2  int not null default 0 comment '操作2',
+    action3  int not null default 0 comment '操作3',
+    action4  int not null default 0 comment '操作4',
+    action5  int not null default 0 comment '操作5',
+    action6  int not null default 0 comment '操作6',
+    action7  int not null default 0 comment '操作7',
+    action8  int not null default 0 comment '操作8',
+    action9  int not null default 0 comment '操作9',
     action10 int not null default 0 comment '操作10'
 ) comment '分类测试题目操作表';
 -- 生成寻找测试题组信息表
 create table find_test_info
 (
-    id       int primary key auto_increment comment '分类题组id',
+    id        int primary key auto_increment comment '分类题组id',
     target_id int comment '人物id',
-    test1_id int comment '测试题1',
-    test2_id int comment '测试题2',
-    test3_id int comment '测试题3',
-    test4_id int comment '测试题4',
-    test5_id int comment '测试题5',
-    test6_id int comment '测试题6',
-    test7_id int comment '测试题7',
-    test8_id int comment '测试题8',
-    status   tinyint not null default 0 comment '状态，0：正常使用 1:禁止使用',
+    test1_id  int comment '测试题1',
+    test2_id  int comment '测试题2',
+    test3_id  int comment '测试题3',
+    test4_id  int comment '测试题4',
+    test5_id  int comment '测试题5',
+    test6_id  int comment '测试题6',
+    test7_id  int comment '测试题7',
+    test8_id  int comment '测试题8',
+    status    tinyint not null default 0 comment '状态，0：正常使用 1:禁止使用',
     foreign key (target_id) references test_base_info (id),
     foreign key (test1_id) references test_base_info (id),
     foreign key (test2_id) references test_base_info (id),
--- src/main/java/com/all/faceRecognition/util/GenerateDataHelper.java
+++ src/main/java/com/all/faceRecognition/util/GenerateDataHelper.java
@@ -5,17 +5,30 @@ import java.util.Random;
 
 public class GenerateDataHelper {
     public static final HashMap<String, Integer> femaleCharacters = new HashMap<String, Integer>() {{
-        put("Angelagbaby", 8);
-        put("迪丽热巴", 36);
-        put("范冰冰", 9);
-        put("杨幂", 46);
-        put("赵丽颖", 9);
+        put("Angelagbaby", 164);
+//        put("蔡文静",103);
+        put("迪丽热巴", 104);
+        put("范冰冰", 102);
+        put("古力娜扎", 107);
+        put("杨幂", 196);
+        put("张子枫", 103);
+        put("赵今麦", 103);
+        put("赵丽颖", 100);
     }};
 
     public static final HashMap<String, Integer> maleCharacters = new HashMap<String, Integer>() {{
-        put("李晨", 12);
-        put("鹿晗", 16);
-        put("郑恺", 20);
+//        put("蔡徐坤",95);
+        put("陈立农", 101);
+        put("李晨", 119);
+//        put("鹿晗", 210);
+        put("王俊凯", 100);
+        put("王一博", 100);
+//        put("肖战", 70);
+        put("许光汉", 84);
+        put("薛之谦", 104);
+        put("张艺兴", 100);
+        put("郑恺", 54);
+        put("朱一龙", 100);
     }};
 
     public static HashMap<String, Integer> getRandomCharacters(HashMap<String, Integer> femaleCharacters, HashMap<String, Integer> maleCharacters) {
